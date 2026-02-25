b54def68f71dc6521c001543516ddff575cd574c
castilloubedaruben <castilloubedaruben001.alu@stemgranada.com>
2026-02-25 14:15:20 +0100

    Hago todos los ejercicios relacion 4 DOM

---
+++ Tema5/Relacion4/js/script.js
@@ -0,0 +1,40 @@
+// ======================== 1 =======================
+const imagenAlerta = document.getElementById("imgClick")
+
+imagenAlerta.addEventListener("click", function() {
+    alert ("¡Siente la Energía de Burn!")
+})
+
+
+// ========================= 2 =======================
+
+const cambiarImagen = document.getElementById("imgHover")
+
+cambiarImagen.addEventListener("mouseover", function () {
+    cambiarImagen.setAttribute("src", "https://picsum.photos/200?random=99")
+})
+
+// ========================= 3 =======================
+
+const teclaPresionada = document.getElementById("campoTexto")
+teclaPresionada.addEventListener("keydown", function(event) {
+    console.log("Tecla presionada: " + event.key)
+})
+
+// ========================= 4 =======================
+
+const enviarFormulario = document.querySelector("button")
+
+enviarFormulario.addEventListener("click", function() {
+    let datosUsuario = document.getElementById("nombre")
+    let datosUsuarioValor = datosUsuario.value
+    const resultado = document.getElementById("resultado")
+    resultado.textContent="Energía recargada, " + datosUsuarioValor
+})
+
+document.getElementById('formulario').addEventListener('submit', function(event) {
+    event.preventDefault();
+})
+
+// ========================= 5 =======================
+
